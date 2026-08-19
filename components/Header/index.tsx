ad4453240249a4e9dee180a7693249a33ab78c41
claudiacarion <claudia.carion@gmail.com>
2026-08-19 16:26:12 +0200

    add Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Subtitle from "../Subtitle";
+import Title from "../Title";
+
+const Header = () => {
+  return (
+    <header>
+      <Title text="Neon Reflex" />
+      <Subtitle text="Click the correct square" />
+    </header>
+  );
+};
+
+export default Header;
